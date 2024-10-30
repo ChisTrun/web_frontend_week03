731591d4f97f626474647f2bedf261d94875eae3
trungvc <kappa@carem.io>
2024-10-30 14:10:01 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv';
-dotenv.config({ path: '.env.development' });
+dotenv.config();
 
 const apiUrl = process.env.VITE_API_DOMAIN;
 
