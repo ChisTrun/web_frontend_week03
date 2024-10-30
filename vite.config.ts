e7e16372f7fae5c662fe96a4782919b96af135f8
trungvc <kappa@carem.io>
2024-10-30 14:24:47 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.development' });
+
+const apiUrl = process.env.VITE_API_DOMAIN;
+
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: "https://web-backend-week03.vercel.app", 
+        target: apiUrl, 
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), 
       },
