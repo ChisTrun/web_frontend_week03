a7799c4f7f675361c39e897fa4ffa88a6c1952f6
Võ Chí Trung <147219006+ChisTrun@users.noreply.github.com>
2024-10-31 10:10:32 +0700

    Update LoginForm.tsx

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+I'mimport { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Login from "../features/users/apis/Login";
 import Cookies from "js-cookie";
@@ -49,7 +49,7 @@ function LoginForm({ setAlert, setFetching }: { setAlert: Function, setFetching:
             </div>
             <div className="flex items-center justify-between mb-4">
                 <button onClick={ () =>{ toRegister()}}
-                    className="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Create
+                    className="text-xs text-red-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Create
                     Account</button>
             </div>
             <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Login</button>
